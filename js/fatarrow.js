817fb84e590bd7ae9c7bb6cd0fc78c2cc3b88690
er84yaabhishek <er84yaabhishek@gmail.com>
2023-02-10 17:48:10 +0530

    this is a fatarrow function

---
+++ js/fatarrow.js
@@ -0,0 +1,9 @@
+// function defination
+//function name are always written in camelCase
+
+function myFunctionA(n){    //nis a formal argument
+    //function body
+    console.log('Hello every one  '+n+', how r u ?')
+}
+// function calling
+myFunctionA("abhishek");
